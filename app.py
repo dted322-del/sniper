4044be00eb9f8ce2223a9a0b5a4fda1fc114ca25
dted322-del <dted322@gmail.com>
2026-03-03 22:42:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,21 @@
 import streamlit as st
-import pandas as pd
-import urllib.parse
-import random
-import datetime
-from PIL import Image
-from pyzbar.pyzbar import decode
-import plotly.express as px
-from fpdf import FPDF
-import io
 
-# 1. STYLE LUXE
-st.set_page_config(page_title=&quot;SNIPER OS PRESTIGE&quot;, page_icon=&quot;��&quot;,
-layout=&quot;wide&quot;)
+# Configuration simple sans symboles speciaux
+st.set_page_config(page_title=&quot;SNIPER OS&quot;, page_icon=&quot;��&quot;)
 
-st.markdown(&quot;&quot;&quot;
-&lt;style&gt;
-.stApp { background-color: #030303; color: white; }
-.glow { color: #00ff00; text-align: center; font-family: sans-serif; }
-&lt;/style&gt;
-&quot;&quot;&quot;, unsafe_allow_stdio=True)
+st.markdown(&quot;&lt;h1 style=&#39;color:green;&#39;&gt;SNIPER OS CONNECTE&lt;/h1&gt;&quot;,
+unsafe_allow_stdio=True)
 
-# 2. LOGIQUE
-@st.cache_data
-def get_img(cat, i):
-return f&quot;https://source.unsplash.com/featured/800x600?{cat},product&amp;sig={i}&quot;
+st.write(&quot;Si tu vois ce message, ton application fonctionne enfin !&quot;)
 
-# 3. INTERFACE
-st.markdown(&quot;&lt;h1 class=&#39;glow&#39;&gt;SNIPER OS ELITE&lt;/h1&gt;&quot;, unsafe_allow_stdio=True)
+# Menu de base
+choix = st.sidebar.radio(&quot;Menu&quot;, [&quot;Radar&quot;, &quot;Profit&quot;])
 
-with st.sidebar:
-st.title(&quot;Scanner&quot;)
-st.camera_input(&quot;Prendre une photo&quot;)
-st.info(&quot;Code : VIBECUT&quot;)
+if choix == &quot;Radar&quot;:
+niche = st.text_input(&quot;Niche a analyser&quot;, &quot;Yoga&quot;)
+st.write(f&quot;Analyse en cours pour : {niche}&quot;)
+st.link_button(&quot;Chercher sur Amazon&quot;, f&quot;https://www.amazon.fr/s?k={niche}&quot;)
 
-tabs = st.tabs([&quot;�� RADAR&quot;, &quot;�� PROFIT&quot;])
-
-with tabs[0]:
-query = st.text_input(&quot;Niche :&quot;, &quot;Fitness&quot;)
-if query:
-st.image(get_img(query, 1), width=300)
-st.link_button(&quot;Rechercher sur Amazon&quot;, f&quot;https://www.amazon.fr/s?k={query}&quot;)
-
-with tabs[1]:
-ca = st.number_input(&quot;CA mensuel vise (€)&quot;, value=10000)
-marge = st.slider(&quot;Marge %&quot;, 10, 50, 25)
-st.metric(&quot;Profit Net&quot;, f&quot;{int(ca * marge / 100)} €&quot;)
+if choix == &quot;Profit&quot;:
+ca = st.number_input(&quot;CA mensuel&quot;, value=10000)
+st.write(f&quot;Estimation de profit : {int(ca * 0.25)} euros&quot;)
